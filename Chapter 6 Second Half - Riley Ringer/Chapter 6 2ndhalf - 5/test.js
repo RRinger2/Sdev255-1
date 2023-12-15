d544edae54deca850b47b6611e8fade41a071ffe
Riley Ringer <2019rinri@gmail.com>
2023-12-14 21:11:24 -0500

    Initial commit

---
+++ Chapter 6 Second Half - Riley Ringer/Chapter 6 2ndhalf - 5/test.js
@@ -0,0 +1,4 @@
+var colors = require("colors");
+console.log(colors.random("First text in random colors"));
+console.log(colors.random("Second text in random colors"));
+console.log(colors.green("This is a green text"));
