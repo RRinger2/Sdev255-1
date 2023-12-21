a33a885c96b3ea184e64ad2493bae022ea69cc2d
Riley Ringer <2019rinri@gmail.com>
2023-12-21 18:03:23 -0500

    Project Submission

---
+++ Login Project/src/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const path = require("path");
+const collection = require("./config");
+const bcrypt = require('bcrypt');
+
+const app = express();
+// convert data into json format
+app.use(express.json());
+// Static file
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+//use EJS as the view engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+// Register User
+app.post("/signup", async (req, res) => {
+
+    const data = {
+        name: req.body.username,
+        password: req.body.password
+    }
+
+    // Check if the username already exists in the database
+    const existingUser = await collection.findOne({ name: data.name });
+
+    if (existingUser) {
+        res.send('User already exists. Please choose a different username.');
+    } else {
+
+        const userdata = await collection.insertMany(data);
+        res.redirect("/");
+        console.log(userdata);
+    }
+
+});
+
+// Login user 
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+        if (!check) {
+            res.send("Username not found")
+        }
+        // Compare passwords
+        const PasswordMatch = await (req.body.password, check.password);
+        if (!PasswordMatch) {
+            res.send("wrong Password");
+        }
+        else {
+            res.render("home");
+        }
+    }
+    catch {
+        res.send("wrong Details");
+    }
+});
+
+
+// Define Port for Application
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
